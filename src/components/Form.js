043e59a0ea2039c8dd0f136a90e3577cda597ac5
Yash-Bhar <89098728+Yash-Bhar@users.noreply.github.com>
2023-06-20 19:28:14 +0530

    Update Form.js
    Corrected functions

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,7 +5,7 @@ import { ethers } from "ethers";
 //const ethers = require('ethers')
 
 const ABI = [
-    [
+    
         {
             "inputs": [],
             "stateMutability": "nonpayable",
@@ -351,41 +351,81 @@ const ABI = [
             "stateMutability": "nonpayable",
             "type": "function"
         }
-    ]
+    
 ]
 
 function Form() {
 
-    const [contractName, setcontractName] = useState('');
-    const [contractSymbol, setcontractSymbol] = useState('');
-    const [contractAmount, setcontractAmount] = useState('');
-    const [walletConnected, setwalletConnected] = useState('');
-    const [contractAddress, setcontractAddress] = useState('');
+    const [tokenName, setTokenName] = useState('');
+    const [tokenSymbol, setTokenSymbol] = useState('');
+    const [totalSupply, setTotalSupply] = useState('');
     const [urBalance, seturBalance] = useState('');
+    const [connectedWallet, setConnectedWallet] = useState('');
+    const [recipientAddress, setRecipientAddress] = useState('');
+    const [amountToSend, setAmountToSend] = useState('');
     const [contractAddress, setcontractAddress] = useState('');
 
     const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const tokenContract = new ethers.Contract(contractAddress, ABI, provider.getsigner());
-    const fetchTokenData = async()=>{
-        try{
-            const name = await tokenContract.name();
-            setcontractName(name);
+    // Create a new instance of the ERC20 token contract
+    const tokenContract = new ethers.Contract(
+        contractAddress,
+        [
+            'function name() view returns (string)',
+            'function symbol() view returns (string)',
+            'function totalSupply() view returns (uint256)',
+            'function balanceOf(address) view returns (uint256)',
+            'function transfer(address, uint256) returns (bool)',
+        ],
+        provider.getSigner()
+    );
 
-            const symbol = await tokenContract.symbol();
-            setcontractSymbol(symbol);
 
-            const totalSupply = await tokenContract.totalSupply();
-            setTotalSupply(totalSupply.toString());
-
-            const address = await provider.getSigner().getAddress();
-            setwalletConnected(address);
-
-            const balance = await tokenContract.balanceof(address);
-            seturBalance(balance);
-        }catch(error){
-            console.log("error", error);
+    const handleSendToken = async () => {
+        try {
+          // Ensure the recipient address and amount to send are provided
+          if (!recipientAddress || !amountToSend) {
+            console.error('Recipient address and amount are required');
+            return;
+          }
+    
+          // Convert the amount to a BigNumber
+          const amount = ethers.utils.parseUnits(amountToSend);
+    
+          // Call the transfer function to send the token to the recipient address
+          const tx = await tokenContract.transfer(recipientAddress, amount);
+    
+          // Wait for the transaction to be mined
+          await tx.wait();
+    
+          console.log('Token transfer successful!');
+        } catch (error) {
+          console.error('Error sending token:', error);
         }
-    }
+    };
+
+    const fetchTokenData = async () => {
+        try {
+          // Fetch the token name
+          const name = await tokenContract.name();
+          setTokenName(name);
+    
+          // Fetch the token symbol
+          const symbol = await tokenContract.symbol();
+          setTokenSymbol(symbol);
+    
+          // Fetch the total supply
+          const totalSupply = await tokenContract.totalSupply();
+          setTotalSupply(totalSupply.toString());
+    
+          const address = await provider.getSigner().getAddress();
+          setConnectedWallet(address);
+    
+          const balance = await tokenContract.balanceOf(address);
+          seturBalance(balance.toString());
+        } catch (error) {
+          console.error('Error fetching token data:', error);
+        }
+    };
   return (
     <>
       <div className='f'>
@@ -394,22 +434,22 @@ function Form() {
                 Token Data
             </div>
             <div className='input-container ic2'>
-                <input type='text' placeholder='Fetching Address' />
+                <input id="email" class="input" onChange={(e) => setcontractAddress(e.target.value)} type="text"  placeholder="Fetching address" />
             </div>
-            <button type='text' className='submit'> Fetch </button>
+            <button type="text" onClick={fetchTokenData}  class="submit">Fetch</button>
         </div>
         <div className='form'>
             <div className='title'>
                 Token Data
             </div>
             <div className='input-container ic2 x'>
-                <li>Name</li>
-                <li>Symbol</li>
-                <li>Total Supply</li>
-                <li>Connected Wallet</li>
-                <li>Balance</li>
+                <li>Name - {tokenName}</li>
+                <li>Symbol - {tokenSymbol}</li>
+                <li>Total Supply - {totalSupply}</li>
+                <li>Connected wallet - {connectedWallet}</li>
+                <li>Balance - {urBalance}</li>
             </div>
-            <button type='text' className='submit'> Refresh </button>
+            <button type="text" onClick={fetchTokenData} class="submit">Refresh</button>
 
         </div>
         <div className='form'>
@@ -417,12 +457,12 @@ function Form() {
                 Send Data
             </div>
             <div className='input-container ic1'>
-                <input type='text' placeholder='Recipient Address' />
+                <input id="firstname" class="input" onChange={(e) => setRecipientAddress(e.target.value)} type="text" placeholder="Recipients address" />
             </div>
             <div className='input-container ic2'>
-                <input type='text' placeholder='Amount to send' />
+                <input id="lastname" class="input" type="text" onChange={(e) => setAmountToSend(e.target.value)} placeholder="Amount to send" />
             </div>
-            <button type='text' className='submit'> Send! </button>
+            <button type="text" onClick={handleSendToken} class="submit">Send</button>
         </div>
       </div>
     </>
